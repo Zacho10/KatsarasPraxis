9facb3ad5f6ba602c25c6b46434aedcddc157ad7
Zachopoulos <znbsystems@Zachopouloss-MacBook-Air.local>
2026-06-28 20:35:13 +0300

    Fix Supabase project URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.KATSARAS_SUPABASE = {
-  url: "https://wmfxyzocfuetxntdpnew.supabase.co/rest/v1/",
+  url: "https://wmfxyzocfuetxntdpnew.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtZnh5em9jZnVldHhudGRwbmV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NjUwOTYsImV4cCI6MjA5ODI0MTA5Nn0.PXwfyBOg0gFVFkBwtk2fQBV3WJ5NfBjWZAFONoB0gF0"
 };
